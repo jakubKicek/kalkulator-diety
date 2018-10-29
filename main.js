0df014719f69ec20b3e32913fd25d9c30f6b3000
Jakub Kicek <j.kicek@gmail.com>
2018-10-29 21:58:01 +0100

    rwd_3

--- main.js
+++ main.js
@@ -1,21 +1,81 @@
 $(document).ready(function(){
 
-    $('#click-btn').click(function(event){
+    $('#click-btn1').click(function(event){
         event.preventDefault();
-        calc();
+        calc1();
     });
-    $('#reset-btn').click(function(){
+    $('#click-btn2').click(function(event){
+        event.preventDefault();
+        calc2();
+    });
+    $('#reset-btn1').click(function(){
+        $('#you-need').text(" ");
+        $('#result-container').text(" ");
+    })
+    $('#reset-btn2').click(function(){
         $('#you-need').text(" ");
         $('#result-container').text(" ");
     })
 })
-calc=()=>{
+calc1=()=>{
+    console.log('calc1');
+    let gender = $('#gender1').val();
+    let age = $('#age1').val();
+    let height = $('#height1').val();
+    let mass = $('#mass1').val();
+    let activity = $('#activity1').val();
+    let genderFactor=0;
+    let activityFactor=0;
+    let result;
     
-    let gender = $('#gender').val();
-    let age = $('#age').val();
-    let height = $('#height').val();
-    let mass = $('#mass').val();
-    let activity = $('#activity').val();
+    // check gender value
+    if(gender=='Mężczyzna'){
+        genderFactor=5;
+    }
+    else if(gender=='Kobieta'){
+        genderFactor=-161;
+    }
+    else{
+        genderFactor=-9999999;
+    }
+    genderFactor = parseInt(genderFactor);
+    
+    
+    //check acitivity value
+    switch(activity){
+        case "pacjent chory, leżący w łóżku":
+            activityFactor=1.25;
+            break;
+        case "niska aktywność fizyczna":
+            activityFactor=1.4;
+            break;
+        case "umiarkowana aktywność fizyczna":
+            activityFactor=1.6;
+            break;
+        case "aktywny tryb życia":
+            activityFactor=1.75;
+            break;
+        case "bardzo aktywny tryb życia":
+            activityFactor=2;
+            break;
+        case "wyczynowe uprawianie sportu":
+            activityFactor=2.3;
+            break;
+        default:
+            genderFactor=-999999;
+    }
+    result=parseInt((10*mass+6.25*height-5*age+genderFactor)*activityFactor);
+
+    
+    printResult(result);
+}
+calc2=()=>{
+    
+    let gender = $('#gender2').val();
+    let age = $('#age2').val();
+    let height = $('#height2').val();
+    let mass = $('#mass2').val();
+    let activity = $('#activity2').val();
     let genderFactor=0;
     let activityFactor=0;
     let result;
